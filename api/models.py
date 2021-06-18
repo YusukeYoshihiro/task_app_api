a1413b1c3a6bd1ba7ca00918a3f39aa8c2fc31a0
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-06-17 17:50:02 -0700

    Changed setting.py and urls.py in task_app_api

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ def upload_avatar_path(instance, filename):
     # 'ext' will be contained 'extension(拡張子)' like JPEG, PNG ...etc
     ext = filename.split('.')[-1]
     # It will create like "(instance.user_profile.id).jpeg" and it will be stored to avatars's folder
-    return '/'.join(['avatars', str(instance.user_profile.id) + str(".") + str(ext)])
+    return '/'.join(['avatars', str(instance.user_profile.id)+str(".")+str(ext)])
 
 
 class Profile(models.Model):
